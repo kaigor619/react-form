b58e04af000e79de057d6d6ad2832c42a07719a4
kaigor619 <38076486+kaigor619@users.noreply.github.com>
2020-05-21 15:47:27 +0300

    Updates

---
+++ precache-manifest.7a10f82053f37c65ee86f10e542e8bbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b9a08673ac86e9585ef484ab3f26bf",
+    "url": "/kaigor619/react-form/index.html"
+  },
+  {
+    "revision": "aa11eb14ddfdddb79fb0",
+    "url": "/kaigor619/react-form/static/css/main.c05960de.chunk.css"
+  },
+  {
+    "revision": "206949f59127895cb479",
+    "url": "/kaigor619/react-form/static/js/2.aeccbd60.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kaigor619/react-form/static/js/2.aeccbd60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa11eb14ddfdddb79fb0",
+    "url": "/kaigor619/react-form/static/js/main.95ab3d90.chunk.js"
+  },
+  {
+    "revision": "7a9628842f77d4c956b5",
+    "url": "/kaigor619/react-form/static/js/runtime-main.4d2d578f.js"
+  },
+  {
+    "revision": "98bf746b14deb46cfe67a37695c39796",
+    "url": "/kaigor619/react-form/static/media/folder.98bf746b.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kaigor619/react-form/precache-manifest.7a10f82053f37c65ee86f10e542e8bbf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kaigor619/react-form/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
