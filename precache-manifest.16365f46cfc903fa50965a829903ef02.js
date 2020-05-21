82e50758e36c46bf89c3a9bd9330744853e1fa17
kaigor619 <38076486+kaigor619@users.noreply.github.com>
2020-05-21 23:45:49 +0300

    Updates

---
+++ precache-manifest.16365f46cfc903fa50965a829903ef02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba05dd0251af6eee766da8e80843735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e11be653896af883764f",
+    "url": "/static/css/main.c05960de.chunk.css"
+  },
+  {
+    "revision": "206949f59127895cb479",
+    "url": "/static/js/2.aeccbd60.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aeccbd60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11be653896af883764f",
+    "url": "/static/js/main.4094c5a3.chunk.js"
+  },
+  {
+    "revision": "b13224f8fce6fa33c354",
+    "url": "/static/js/runtime-main.ea970eac.js"
+  },
+  {
+    "revision": "98bf746b14deb46cfe67a37695c39796",
+    "url": "/static/media/folder.98bf746b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-form/precache-manifest.fca98afa5ced49b0226ca8bf907e5480.js"
+  "/precache-manifest.16365f46cfc903fa50965a829903ef02.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-form/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
